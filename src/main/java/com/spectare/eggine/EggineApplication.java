37dd071c8f380fec35cb018d4b1fa30a3f62fefa
azdragon2 <azdragon2@hotmail.com>
2020-01-23 22:39:48 -0800

    initial commit

---
+++ src/main/java/com/spectare/eggine/EggineApplication.java
@@ -0,0 +1,13 @@
+package com.spectare.eggine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EggineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spectare/eggine/EggineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spectare.eggine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EggineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
